405a77e88b9cbf7a0335bda649fe44f96a0dc554
Robert Smith <smith.rob84@gmail.com>
2017-07-02 22:42:13 -0400

    Initial version

---
+++ Private/Get-Element.ps1
@@ -0,0 +1,19 @@
+function Get-Element {
+    param (
+        $IEObject,
+        [string]$TagName,
+        [hashtable]$Attributes
+    )
+    
+    # Wrap results in array so that the Where method is available which processes faster than Where-Object.
+    $elements = @($IEObject.Document.IHTMLDocument3_getElementsByTagName($TagName))
+    $array = New-Object -TypeName System.Collections.ArrayList
+    $template = '$_.{0} -eq "{1}"'
+    foreach ($key in $Attributes.Keys) {
+        $condition = $template -f $key, $Attributes.$key
+        $array.Add($condition) | Out-Null
+    }
+    $scriptBlock = [scriptblock]::Create($array -join ' -and ')
+    #return
+    $elements.Where($scriptBlock)
+}
