13d843e4527c18a9dfd027b3f7b27c9cd755132b
Robert Smith <smith.rob84@gmail.com>
2017-07-02 22:42:00 -0400

    Fill in code
    Filled in the actual code for the function. Currently only returns an object with the oauth_token and oauth_token_secret. Will add the option to securely save everything to a file next.

--- Public/Request-EtsyOauthToken.ps1
+++ Public/Request-EtsyOauthToken.ps1
@@ -6,4 +6,42 @@ function Request-EtsyOauthToken {
         [string]$ConsumerSecret,
         [switch]$DoNotStore
     )
+    $requestSplat = @{
+        Uri = 'https://openapi.etsy.com/v2/oauth/request_token'
+        ConsumerKey = $ConsumerKey
+        ConsumerSecret = $ConsumerSecret
+        Method = 'POST'
+    }
+    $confirmUrl = Invoke-OAuthMethod @requestSplat
+    $decodedUrl = [Web.HttpUtility]::UrlDecode($confirmUrl.Substring(10))
+    $tempTokens = Get-TempToken -Response $decodedUrl
+    $window = Show-OauthWindow -Url $decodedUrl
+    # Wait until page is loaded before looking for the element
+    Start-Sleep -Seconds 1
+    $waitSplat = @{
+        IEObject = $window
+        TagName = 'DIV'
+        Attributes = @{
+            ClassName = 'oauth-verifier'
+        }
+    }
+    $oauthVerifier = Wait-ForElement @waitSplat
+    $window.Quit()
+    $tokensSplat = @{
+        Uri = 'https://openapi.etsy.com/v2/oauth/access_token'
+        ConsumerKey = $ConsumerKey
+        ConsumerSecret = $ConsumerSecret
+        Token = $tempTokens.oauth_token
+        TokenSecret = $tempTokens.oauth_token_secret
+        Method = 'POST'
+        Verifier = $oauthVerifier.textContent
+    }
+    $tokens = (Invoke-OAuthMethod @tokensSplat).Split('&')
+    $obj = New-Object -TypeName PSCustomObject
+    foreach ($token in $tokens) {
+        $pieces = $token.Split('=')
+        $obj | Add-Member -MemberType NoteProperty -Name $pieces[0] -Value $pieces[1]
+    }
+    #return
+    $obj
 }
