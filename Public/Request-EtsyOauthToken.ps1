8371973d2edf23878ff965882f626f9ce21e267e
Robert Smith <smith.rob84@gmail.com>
2017-07-03 23:27:30 -0400

    Store tokens securely
    ConsumerKey and ConsumerSecret are now each required to be of type SecureString. All tokens are now stored in PSCredential objects when returned or exported. Garbage collection is also invoked as soon as $tokens is no longer needed to make sure the tokens do not stay in memory.

--- Public/Request-EtsyOauthToken.ps1
+++ Public/Request-EtsyOauthToken.ps1
@@ -1,15 +1,18 @@
 function Request-EtsyOauthToken {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$ConsumerKey,
+        [securestring]$ConsumerKey,
         [Parameter(Mandatory=$true)]
-        [string]$ConsumerSecret,
+        [securestring]$ConsumerSecret,
         [switch]$DoNotStore
     )
+    $consumerKeyCred = New-Object -TypeName PSCredential -ArgumentList 'consumer_key',$ConsumerKey
+    $consumerSecretCred = New-Object -TypeName PSCredential -ArgumentList 'consumer_secret',$ConsumerSecret
+
     $requestSplat = @{
         Uri = 'https://openapi.etsy.com/v2/oauth/request_token'
-        ConsumerKey = $ConsumerKey
-        ConsumerSecret = $ConsumerSecret
+        ConsumerKey = $consumerKeyCred.GetNetworkCredential().password
+        ConsumerSecret = $consumerSecretCred.GetNetworkCredential().password
         Method = 'POST'
     }
     $confirmUrl = Invoke-OAuthMethod @requestSplat
@@ -29,19 +32,33 @@ function Request-EtsyOauthToken {
     $window.Quit()
     $tokensSplat = @{
         Uri = 'https://openapi.etsy.com/v2/oauth/access_token'
-        ConsumerKey = $ConsumerKey
-        ConsumerSecret = $ConsumerSecret
+        ConsumerKey = $consumerKeyCred.GetNetworkCredential().password
+        ConsumerSecret = $consumerSecretCred.GetNetworkCredential().password
         Token = $tempTokens.oauth_token
         TokenSecret = $tempTokens.oauth_token_secret
         Method = 'POST'
         Verifier = $oauthVerifier.textContent
     }
     $tokens = (Invoke-OAuthMethod @tokensSplat).Split('&')
-    $obj = New-Object -TypeName PSCustomObject
+    $tokenObj = New-Object -TypeName PSCustomObject
     foreach ($token in $tokens) {
         $pieces = $token.Split('=')
-        $obj | Add-Member -MemberType NoteProperty -Name $pieces[0] -Value $pieces[1]
+        $secure = ConvertTo-SecureString -String $pieces[1] -AsPlainText -Force
+        $creds = New-Object -TypeName PSCredential -ArgumentList $pieces[0],$secure
+        $tokenObj | Add-Member -MemberType NoteProperty -Name $pieces[0] -Value $creds
     }
-    #return
-    $obj
+    #Cleanup tokens from memory
+    $tokens = $null
+    [gc]::Collect()
+    
+    $tokenObj | Add-Member -MemberType NoteProperty -Name consumer_key -Value $consumerKeyCred
+    $tokenObj | Add-Member -MemberType NoteProperty -Name consumer_secret -Value $consumerSecretCred
+
+    if ($DoNotStore -eq $true) {
+        #return
+        $tokenObj
+    }
+    else {
+        Save-SecureToken -Token $tokenObj
+    }
 }
