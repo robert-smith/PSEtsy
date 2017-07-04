eabfc5de68c474b1e32e521de594203073e071da
Robert Smith <smith.rob84@gmail.com>
2017-07-03 23:24:00 -0400

    Make window come to foreground

--- Private/Show-OauthWindow.ps1
+++ Private/Show-OauthWindow.ps1
@@ -7,5 +7,31 @@ Function Show-OauthWindow
     $ie = New-Object -ComObject internetExplorer.Application
     $ie.Navigate($Url)
     $ie.Visible = $true
+    
+    $nativeHelperTypeDefinition =
+@"
+        using System;
+        using System.Runtime.InteropServices;
+
+        public static class NativeHelper
+            {
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+            public static bool SetForeground(IntPtr windowHandle)
+            {
+            return NativeHelper.SetForegroundWindow(windowHandle);
+            }
+
+        }
+"@
+    if(-not ([System.Management.Automation.PSTypeName] "NativeHelper").Type) {
+        Add-Type -TypeDefinition $nativeHelperTypeDefinition
+    }
+
+    [NativeHelper]::SetForeground($ie.HWND) > $null
+
+    #return
     $ie
 }
