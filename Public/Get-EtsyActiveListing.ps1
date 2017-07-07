51b5beaa3e3258992a27ec5f8ca1c200af887b9e
Robert Smith <smith.rob84@gmail.com>
2017-07-06 23:47:37 -0400

    Add documentation and default param values
    $EtsyTokens is a variable created upon the module being imported. It contains tokens that have been stored using Request-OauthToken.

--- Public/Get-EtsyActiveListing.ps1
+++ Public/Get-EtsyActiveListing.ps1
@@ -1,9 +1,42 @@
+<#
+.SYNOPSIS
+Retrieves all Etsy listings for the current user.
+
+.DESCRIPTION
+Outputs an array containing all of the user's Etsy listings.
+
+.PARAMETER ConsumerKey
+A PSCredential object containing the consumer key as the password. The username can be anything.
+
+.PARAMETER ConsumerSecret
+A PSCredential object containing the consumer secret as the password. The username can be anything.
+
+.PARAMETER Token
+A PSCredential object containing the OAuth token as the password. This is received through the OAuth token request process. The username can be anything.
+
+.PARAMETER TokenSecret
+A PSCredential object containing the OAuth token secret as the password. This is received through the OAuth token request process. The username can be anything.
+
+.EXAMPLE
+$splat =
+@{
+    ConsumerKey = $ConsumerKeyCredential
+    ConsumerSecret = $ConsumerKeyCredential
+    Token = $TokenCredential
+    TokenSecret = $TokenSecretCredential
+}
+
+PS C:\> Get-EtsyActiveListing @splat
+
+.OUTPUTS
+PSCustomObject
+#>
 function Get-EtsyActiveListing {
     param (
-        [PSCredential]$ConsumerKey,
-        [PSCredential]$ConsumerSecret,
-        [PSCredential]$Token,
-        [PSCredential]$TokenSecret
+        [PSCredential]$ConsumerKey = $EtsyTokens.consumer_key,
+        [PSCredential]$ConsumerSecret = $EtsyTokens.consumer_secret,
+        [PSCredential]$Token = $EtsyTokens.oauth_token,
+        [PSCredential]$TokenSecret = $EtsyTokens.oauth_token_secret
     )
     $splat = @{
         ConsumerKey = $ConsumerKey
